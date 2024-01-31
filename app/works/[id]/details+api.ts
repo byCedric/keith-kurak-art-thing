614bfc55c2e0079d53d1d92a1f689c78a5cc94e2
Cedric van Putten <github@cedric.dev>
2024-01-31 16:16:07 +0100

    refactor: add error handling for work details api route

--- app/works/[id]/details+api.ts
+++ app/works/[id]/details+api.ts
@@ -1,13 +1,19 @@
-import { ExpoRequest, ExpoResponse } from 'expo-router/server';
+import { type ExpoRequest, ExpoResponse } from 'expo-router/server';
+
+import cachedBaskets from '~/local-data/Basketry.json';
 
 export async function GET(request: ExpoRequest) {
+  // Abort if no id is provided
   const id = request.expoUrl.searchParams.get('id');
-  const localData = require('~/local-data/Basketry.json');
-  const localRecord = localData.data.find((record: any) => record.id === id);
-  if (localRecord) {
-    return ExpoResponse.json({ data: localRecord });
-  }
+  if (!id) return ExpoResponse.json({ error: 'No id provided' }, { status: 400 });
+
+  // Return cached data if available
+  const basket = cachedBaskets.data.find((record: any) => record.id === id);
+  if (basket) return ExpoResponse.json({ data: basket });
+
+  // Otherwise, try to fetch from the API
   const response = await fetch(`https://openaccess-api.clevelandart.org/api/artworks/${id}`);
-  const responseJson = await response.json();
-  return ExpoResponse.json(responseJson);
+  return response.ok
+    ? Response.json(await response.json())
+    : Response.json({ error: `Cleveland Art returned: ${response.status}` }, response);
 }
